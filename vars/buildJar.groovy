4f67afd1ee7122ae0ee2909b7e58b107dad30168
aamdevsecops <aamdevsecops@localhost>
2022-12-29 13:21:10 +0100

    added vars for groovy

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,8 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo "building the application for branch $BRANCH_NAME"
-    sh 'mvn package'
+
+    echo "building the application..."
+    sh 'mvn package
 }
+
